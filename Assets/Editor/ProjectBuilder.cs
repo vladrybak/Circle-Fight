c760542231bf959a8fd049b7cec8d36c59740ec9
vladrybak <gg.vladrybak@gmail.com>
2019-09-28 21:17:30 +0300

    ProjectBuilder for cloud build

---
+++ Assets/Editor/ProjectBuilder.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ProjectBuilder : MonoBehaviour
+{
+    static void PerformBuild() {
+        var scenes = EditorBuildSettings.scenes;
+        string[] sceneNames = new string[scenes.Length];
+        for(int i=0;i<scenes.Length;i++) {
+            sceneNames[i] = scenes[i].path;
+            Debug.Log("Scene added to build:" + sceneNames[i]);
+        }
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = sceneNames;
+        Debug.Log("Start building player");
+        BuildPipeline.BuildPlayer(options);
+        Debug.Log("Building complete");
+    }
+}
